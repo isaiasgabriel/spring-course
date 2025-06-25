16459f58e35716f8b9aa83a1c5e6428a96ed1381
Isaías Gabriel <is.gabri3l@gmail.com>
2025-06-25 16:33:57 -0300

    refactor: move object initialization to @PostConstruct method

--- src/main/java/com/isaiasgabriel/rest_demo/rest/StudentRestController.java
+++ src/main/java/com/isaiasgabriel/rest_demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.isaiasgabriel.rest_demo.rest;
 
 import com.isaiasgabriel.rest_demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,25 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    // Declare students list
+    private List<Student> students;
 
-        List<Student> students = new ArrayList<>();
+    // @PostConstruct executes once after it's created by spring
+    @PostConstruct
+    public void loadData() {
+        // Initialize students list
+        // Using this.students is optional here because there's no name conflict
+        // So we're cleary referring to the students attribute above
+        students = new ArrayList<>();
 
         students.add(new Student("Alexander", "Volkanovski"));
         students.add(new Student("Alex", "Poatan"));
         students.add(new Student("Kai-kara", "France"));
         students.add(new Student("Zabit", "Magomedsharipov"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
     }
 }

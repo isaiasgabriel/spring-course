1c5c4debd1150424881039501fd4e37cbef3dd12
Isaías Gabriel <is.gabri3l@gmail.com>
2025-06-25 08:34:05 -0300

    feat: implement student dao
    DAO > Data Access Object

--- src/main/java/com/isaiasgabriel/cruddemo/CruddemoApplication.java
+++ src/main/java/com/isaiasgabriel/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.isaiasgabriel.cruddemo;
 
+import com.isaiasgabriel.cruddemo.dao.StudentDAO;
+import com.isaiasgabriel.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,21 @@ public class CruddemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner ->{
-            System.out.println("Hello World");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+       System.out.println("Creating new student ...");
+       Student student = new Student("Paul", "Richard", "paulrichard@gmail.com");
+
+       System.out.println("Saving the student ...");
+       studentDAO.save(student);
+
+        System.out.println("Student saved successfully. ID: " + student);
+
+
     }
 }
---
+++ src/main/java/com/isaiasgabriel/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.isaiasgabriel.cruddemo.dao;
+
+import com.isaiasgabriel.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save (Student student);
+}

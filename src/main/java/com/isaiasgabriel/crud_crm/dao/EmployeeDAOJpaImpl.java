136aeb88c3a6be75f9bce0cac257bdad8e9adef3
Isaías Gabriel <is.gabri3l@gmail.com>
2025-06-26 16:46:24 -0300

    feat(Employee): add findById, save and delete DAO

--- src/main/java/com/isaiasgabriel/crud_crm/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/isaiasgabriel/crud_crm/dao/EmployeeDAOJpaImpl.java
@@ -33,4 +33,42 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
         // Return the results
         return employees;
     }
+
+    @Override
+    public Employee findById(int id) {
+        // 1. Get employee
+        // Employee employee = entityManager.find(Employee.class, id);
+        // 2. Return employee:
+        // return employee;
+        //
+        // Same as:
+        return entityManager.find(Employee.class, id);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        // 1. Save employee:
+        // Employee dbEmployee = entityManager.merge(employee);
+        //
+        // if id == 0 then INSERT
+        // if id != 0 then UPDATE
+        //
+        // 2. Return the employee
+        // return dbEmployee;
+        //
+        // Same as:
+        return entityManager.merge(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        // 1. Find the employee:
+        // Employee employee = entityManager.find(Employee.class, id);
+        //
+        // 2. Delete the employee:
+        // entityManager.remove(employee);
+        //
+        // Same as:
+        entityManager.remove(entityManager.find(Employee.class, id));
+    }
 }

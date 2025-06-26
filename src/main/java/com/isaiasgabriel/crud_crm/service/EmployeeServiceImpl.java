7d577f359441d5e6e5b183391d3fb01b4c315cbe
Isaías Gabriel <is.gabri3l@gmail.com>
2025-06-26 16:51:03 -0300

    feat(Employee): implement new DAO methods on the service layer
    Don't forget to add the @Transactional annotation on the service
    implementation, we're doing this because it's the service who's
    responsible to manage the DB transactions, not the DAO.

--- src/main/java/com/isaiasgabriel/crud_crm/service/EmployeeServiceImpl.java
+++ src/main/java/com/isaiasgabriel/crud_crm/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.isaiasgabriel.crud_crm.service;
 
 import com.isaiasgabriel.crud_crm.dao.EmployeeDAO;
 import com.isaiasgabriel.crud_crm.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +21,23 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+
+    @Transactional
+    @Override
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
     }
 }

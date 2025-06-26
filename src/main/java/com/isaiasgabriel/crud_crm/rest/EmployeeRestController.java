6768f9d4f529abb65e9bac3e854c0e40608ad8f5
Isaías Gabriel <is.gabri3l@gmail.com>
2025-06-26 17:20:54 -0300

    feat(Employee): implement new service methods on the REST controller

--- src/main/java/com/isaiasgabriel/crud_crm/rest/EmployeeRestController.java
+++ src/main/java/com/isaiasgabriel/crud_crm/rest/EmployeeRestController.java
@@ -1,12 +1,9 @@
 package com.isaiasgabriel.crud_crm.rest;
 
-import com.isaiasgabriel.crud_crm.dao.EmployeeDAO;
 import com.isaiasgabriel.crud_crm.entity.Employee;
 import com.isaiasgabriel.crud_crm.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,4 +23,43 @@ public class EmployeeRestController {
     public List<Employee> getAllEmployees() {
         return  employeeService.findAll();
     }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable int employeeId) {
+            Employee employee = employeeService.findById(employeeId);
+
+            if(employee == null){
+                throw new RuntimeException("Employee with id " + employeeId + " not found");
+            }
+
+            return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(null);
+       // Just in case someone pass an ID inside the JSON.body
+       // However if you are using an in inside the entity then
+       // You should use setId(0);
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployeeById(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null){
+            throw new RuntimeException("Employee with id " + employeeId + " not found");
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id:" + employeeId;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
 }
